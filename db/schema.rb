780a5b3737dfee7de9b774ecf0ea0ce50b6783bd
alpha-blok-app-code <info@businessinsighter.com>
2016-05-16 14:36:19 +0000

    Updated create

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512151335) do
+ActiveRecord::Schema.define(version: 20160516142621) do
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
+    t.string   "title"
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
